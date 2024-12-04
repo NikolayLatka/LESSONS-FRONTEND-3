8a190f54603805e03a74553d9718ed46d9b526b4
NikolayLatka <latkaa4@gmail.com>
2024-12-04 19:28:59 +0200

    add-lesson-4-and-css

---
+++ js/lesson-4.js
@@ -0,0 +1,19 @@
+// Попросити число
+
+let number = prompt("Write any number");
+
+number = Number(number);
+
+// Попросити ступінь
+
+const power = prompt("Write any power");
+
+const modifePower = Number(power);
+
+// Порахувати ступінь
+
+const result = number ** modifePower;
+
+// Вивести результат
+
+alert(result);
